873a10053d86b527b58e8f02f8998962572e8803
DAdhikary06 <166974220+DAdhikary06@users.noreply.github.com>
2025-01-21 19:39:41 +0530

    some  changes in requirment.txt

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 pip install --upgrade pip
-pip install -r requirements_py311.txt
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
