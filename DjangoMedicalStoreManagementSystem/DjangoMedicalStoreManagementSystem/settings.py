e8d2b542f2985b57faaf02df99040854f7689824
DAdhikary06 <166974220+DAdhikary06@users.noreply.github.com>
2025-01-22 11:36:48 +0530

    Final Update

--- DjangoMedicalStoreManagementSystem/DjangoMedicalStoreManagementSystem/settings.py
+++ DjangoMedicalStoreManagementSystem/DjangoMedicalStoreManagementSystem/settings.py
@@ -98,10 +98,12 @@ WSGI_APPLICATION = 'DjangoMedicalStoreManagementSystem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+        'OPTIONS': {
+            'timeout': 20,  # Increase timeout to 20 seconds
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
